f6ce6a39edca0e42b7dbbb633b3803effb7e4d16
hagarg123 <114971064+hagarg123@users.noreply.github.com>
2022-10-20 00:15:58 +0530

    Add files via upload

---
+++ Harshit Sprint_2/CUT/Code/Header/suite.h
@@ -0,0 +1,27 @@
+#include "CUnit/Basic.h"
+#include "function.h"
+
+int init_suite_writetofile(void);
+int clean_suite_writetofile(void);
+
+int init_suite_modify(void);
+int clean_suite_modify(void);
+
+int init_suite_delete(void);
+int clean_suite_delete(void);
+
+int init_suite_translate(void);
+int clean_suite_translate(void);
+
+void writetofile_sunnycases(void);
+void writetofile_rainycases(void);
+
+void modify_sunnycases(void);
+void modify_rainycases(void);
+
+
+void delete_sunnycases(void);
+void delete_rainycases(void);
+
+void translate_sunnycases(void);
+void translate_rainycases(void);
